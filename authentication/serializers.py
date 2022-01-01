7a11f67f5ace5a32f3175d9d7cb89c51e0310dfc
ashishsorte123 <88495557+ashishsorte123@users.noreply.github.com>
2022-01-01 19:38:46 +0530

    first commit

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,35 +1,25 @@
-from rest_framework import fields, serializers
+from rest_framework import serializers
 from django.contrib.auth.models import User
+
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
-        max_length=65, min_length=8, write_only=True)
+        max_length = 65, min_length = 8, write_only = True)
     email = serializers.EmailField(max_length = 255, min_length = 4),
-    first_name =serializers.CharField(max_length = 255, min_length = 2),
-    last_name =serializers.CharField(max_length = 255, min_length = 2),
+    first_name = serializers.CharField(max_length = 255, min_length = 2),
+    last_name = serializers.CharField(max_length = 255, min_length = 2),
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'password']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password'
+                  ]
 
-        def validate(self, attrs):
-            email = attrs.get('email', '')
-            if User.objects.filter(email=email).exists():
-                raise serializers.ValidationError(
-                    {'email': ('Email is already in use')}
-                )
-            return super().validate(attrs)
-        
-        def create(self, validated_data):
-            return User.objects.create_user(**validated_data)
+    def validate(self, attrs):
+        email = attrs.get('email', '')
+        if User.objects.filter(email = email).exists():
+            raise serializers.ValidationError(
+                {'email', ('Email already in use.')})
+        return super().validate(attrs)
 
-    class LoginSerializer(serializers.ModelSerializer):
-        password = serializers.CharField(max_length = 65, min_length = 8, write_only = True)
-        username = serializers.CharField(max_length = 255, min_length = 2)
-
-        class Meta:
-            model = User
-            fields = ['username', 'password'] 
-
-
-
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
